9e9b1a71aecb01e3b4e46b365027d7a36bc8ca0e
GabrielAPaiva <paivagames10@gmail.com>
2021-07-06 23:28:56 -0300

    Configuração de tratamento de erros 2

--- src/main/java/com/example/demo/handlers/FormErrorDTO.java
+++ src/main/java/com/example/demo/handlers/FormErrorDTO.java
@@ -1,12 +1,8 @@
 package com.example.demo.handlers;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
 public class FormErrorDTO {
 
     private String field;
@@ -14,4 +10,42 @@ public class FormErrorDTO {
     private int status;
     private LocalDateTime timeStamp;
 
+    public FormErrorDTO(String field, String mensagem, int status, LocalDateTime time) {
+        this.field = field;
+        this.error = mensagem;
+        this.status = status;
+        this.timeStamp = time;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
